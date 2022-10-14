67c3081d3d99114c8bcbd2cfeceda5adf44dbc49
suyeon woo <woosean999@gmail.com>
2022-10-14 13:45:01 +0900

    1904 01타일

---
+++ suyeon/1904/1/solution.py
@@ -0,0 +1,12 @@
+#1904 01타일
+#started at 10:11
+N = int(input())
+C = 15746
+
+dp = [0,1,2]
+for i in range(3,N+1):
+    sum = dp[i-2] + dp[i-1]
+    dp.append((dp[i-2] % C + dp[i-1] % C) % C)  # (a + b) mod c = (a mod c + b mod c) mod c
+
+print(dp[N])
+#finished at 10:30
