205937bfef6afb770c718f1cb28ace64fed6842a
suyeon woo <woosean999@gmail.com>
2022-10-14 15:18:53 +0900

    1904 불필요한 코드 삭제

--- suyeon/1904/1/solution.py
+++ suyeon/1904/1/solution.py
@@ -5,7 +5,6 @@ C = 15746
 
 dp = [0,1,2]
 for i in range(3,N+1):
-    sum = dp[i-2] + dp[i-1]
     dp.append((dp[i-2] % C + dp[i-1] % C) % C)  # (a + b) mod c = (a mod c + b mod c) mod c
 
 print(dp[N])
