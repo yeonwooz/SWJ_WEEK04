6d45caaa9d8d70efeb2baaa34b5a35dfb5ec61ed
suyeon woo <woosean999@gmail.com>
2022-10-17 19:08:23 +0900

    [WIP]1700 - 2

--- suyeon/1700/1/solution.py
+++ suyeon/1700/1/solution.py
@@ -7,34 +7,38 @@ n,k = map(int, sys.stdin.readline().split())
 
 itemq = deque(list(map(int, sys.stdin.readline().split())))
 socketq = deque([], maxlen = n) # 0구부터 n-1구까지 사용. 사용중인 정수가 들어감 
-cnt = 0
 tempq = deque()
+cnt = 0
 
 while itemq:
     popped = itemq.popleft()
-    if len(socketq) < n:
-        # 빈소켓 존재
-        socketq.append(popped)
+    if popped in socketq:
+        # 이미 아이템이 소켓에 연결되어있다면 스킵, 
+        continue
     else:
-        # 소켓 꽉참
-        if popped in socketq:
-            # 이미 아이템이 연결되어있다면 스킵, 
-            continue
+        # 아이템을 새로 꽂아야한다 
+        if len(socketq) < n:
+            # 빈소켓 존재
+            socketq.append(popped)
+            # 빈 소켓에 꽂는다 
         else:
             # 새로운 아이템을 연결해야한다면 어떤 걸 빼야 할까?   
-            tempq.append(popped)
-            schedule = 1
-            while schedule <= k:
+            tempq.append(popped) # 소켓이 비어있지 않고 popped가 기존에 연결중인 아이템도 아닐 때
+            templen = 1
+            while True:
                 if not itemq: break
-                cur_pop = itemq.popleft()
-                if cur_pop in socketq: 
+                if templen == n: break
+
+                popped = itemq.popleft()
+                if popped in socketq:
                     continue
                 else:
-                    tempq.append(cur_pop)
-                    schedule += 1
+                    tempq.append(popped)
+                templen += 1
 
             while tempq:
-                socketq.append(tempq.popleft())
+                #tempq에 있는 것들을 소켓에 꽂는다.
+                cur_pop = tempq.popleft()
+                socketq.append(cur_pop)
                 cnt += 1
-
 print(cnt)
