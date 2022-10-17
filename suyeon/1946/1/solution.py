6d5ae11f0f9204eb3a4e184be888befd2b98684d
suyeon woo <woosean999@gmail.com>
2022-10-17 15:31:29 +0900

    1946 신입사원 / greedy

--- suyeon/1946/1/solution.py
+++ suyeon/1946/1/solution.py
@@ -13,17 +13,16 @@ for _ in range(T):
     
     people.sort(key = lambda d:(d[0]))  # 뒷사람들로 갈수록 a 랭크가 낮아진다
     
-    max_cnt = 0
-    for i in range(n):
-        cnt = 1
-        high_b_rank = people[i][1] # 그룸에 들어올 뒷사람들의 b 랭크가 high_b_rank보다 높아야 한다 
-        for j in range(i+1, n):
-            cur_b_rank = people[j][1]
-            if high_b_rank > cur_b_rank: # 뒷사람의 b 랭크가 더 높다면
-                cnt += 1
-                high_b_rank = cur_b_rank
-        max_cnt = max(max_cnt, cnt)
-        if max_cnt == n: 
-            break
+    # max_cnt = 0
+    # for i in range(n):
+    cnt = 1
+    high_b_rank = people[0][1] 
+    # 그룹에 들어올 뒷사람들의 b 랭크가 high_b_rank보다 높아야 한다 
+    # a 랭크가 가장 높은 사람은 일단 합격(?)
+    for j in range(1, n):
+        cur_b_rank = people[j][1]
+        if high_b_rank > cur_b_rank: # 뒷사람의 b 랭크가 더 높다면
+            cnt += 1
+            high_b_rank = cur_b_rank
 
-    print(max_cnt)
+    print(cnt)
