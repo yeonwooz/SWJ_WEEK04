4aa6868286160f991a43eb0545931b1302a1b1ca
suyeon woo <woosean999@gmail.com>
2022-10-17 15:38:02 +0900

    1946 신입사원 / 시간초과 해결, 주석 추가

--- suyeon/1946/1/solution.py
+++ suyeon/1946/1/solution.py
@@ -16,13 +16,16 @@ for _ in range(T):
     # max_cnt = 0
     # for i in range(n):
     cnt = 1
-    high_b_rank = people[0][1] 
-    # 그룹에 들어올 뒷사람들의 b 랭크가 high_b_rank보다 높아야 한다 
-    # a 랭크가 가장 높은 사람은 일단 합격(?)
+    row_b_rank = people[0][1]  # 하나라도 1등을 하면 반드시 뽑히게 되어있다.  그 영역에서 그사람보다 잘한 사람이 없기 때문에.
+
+    # 그룹에 들어올 뒷사람들의 b 랭크가 row_b_rank (현재까지 최저 b랭크. 즉 마지노선)보다 높아야 한다 
+    
     for j in range(1, n):
         cur_b_rank = people[j][1]
-        if high_b_rank > cur_b_rank: # 뒷사람의 b 랭크가 더 높다면
+        if row_b_rank > cur_b_rank: # 뒷사람의 b 랭크가 더 높다면
             cnt += 1
-            high_b_rank = cur_b_rank
+            row_b_rank = cur_b_rank
 
     print(cnt)
+
+# finished at 3:30 => 하나라도 1등을 하면 반드시 뽑히게 되어있다는 성질을 이용해야 시간초과가 나지 않는다
