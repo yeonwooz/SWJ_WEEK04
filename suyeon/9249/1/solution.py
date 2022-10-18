03d6db6d881feab4f49917815c0b497e6b1b7f6c
suyeon woo <woosean999@gmail.com>
2022-10-18 21:52:51 +0900

    [WIP]9249 - 블로그 코드 가져옴

--- suyeon/9249/1/solution.py
+++ suyeon/9249/1/solution.py
@@ -1,64 +1,69 @@
-#started at 1:57
 import sys
-from unittest.util import _MAX_LENGTH
-str1 = sys.stdin.readline().rstrip() 
-str2 = sys.stdin.readline().rstrip()
-len1 = len(str1) # 열  # 5
-len2 = len(str2) # 행  # 6
+input = sys.stdin.readline
 
-DP = [[0] * (len1+1) for _ in range(len2+1)]
-for i in range(1, len2+1):
-    for j in range(1, len1+1):
-        if str2[i-1] == str1[j-1]:
-            DP[i][j] = DP[i-1][j-1] + 1
+A = input()
+B = input()
+
+s = A + '1' + B   #두 문자열을 구분짓기 위해 (인덱스가 넘어가지 않게) 특수문자나 숫자를 추가한 후 연결
+n = len(s)
+
+suffix = [i for i in range(n)]
+
+g = [0] * (n+1)  # 순위
+ng = [0] * (n+1) # 갱신순위
+
+for i in range(n):
+    g[i] = ord(s[i])
+
+g[n] = -1
+ng[suffix[0]] = 0 
+ng[n] = -1
+
+t = 1
+while t < n:
+    suffix.sort(key = lambda x: (g[x], g[min(x+t, n)]))
+
+    for i in range(1,n):
+        p,q = suffix[i-1], suffix[i] # 접미사 배열의 앞뒤 비교
+
+        if g[p] != g[q] or g[min(p + t, n)] != g[min(q + t, n)]:
+            ng[q] = ng[p] + 1
+            # 우선순위 변경
         else:
-            DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+            ng[q] = ng[p]
+            # 우선순위 그대로 받아옴
 
-max_cnt = 0
-max_lcs = ''
+    if ng[n-1] == n-1:
+        break
 
-# # 대각선 기준 위영역
-for diff in range(len1):
-    cnt = 0
-    lcs = ''
-    for i in range(1, len2):
-        j = i + diff
-        if  i > len1 or j > len2: continue
-        if i <= len2 and j <= len1 and str2[i-1] == str1[j-1]:
-            cnt += 1
-            lcs += str1[j-1]
-        else:
-            if cnt > max_cnt:
-                max_cnt = cnt
-                max_lcs = lcs    
-            cnt = 0
-            lcs = ''
 
-    if cnt > max_cnt:
-        max_cnt = cnt
-        max_lcs = lcs    
-       
-# # # 대각선 기준 아래 영역
-for diff in range(len2):   
-    cnt = 0
-    lcs = ''
-    for j in range(1, len1):
-        i = j + diff
-        if i > len2 or j > len1: continue
-        if i <= len2 and j <= len1 and str2[i-1] == str1[j-1]:
-            cnt += 1
-            lcs += str1[j-1]
-        else:
-            if cnt > max_cnt:
-                max_cnt = cnt
-                max_lcs = lcs    
-            cnt = 0
-            lcs = ''
+    t = t * 2
+    g = ng[:]
 
-    if cnt > max_cnt:
-        max_cnt = cnt
-        max_lcs = lcs   
+LCP = [0] * n
+length = 0
 
-print(max_cnt)
-print(max_lcs)
-#finished at 3:18 => 메모리 초과
+for i in range(n):
+    k = g[i]
+    if k == 0:
+        # 처음은 들어가지 않는다
+        continue
+    p = suffix[k-1]
+
+    while i + length < n and p + length < n and s[i + length] ==  s[p + length]:
+        length += 1
+    LCP[k] = length
+
+    if length:
+        length -= 1
+
+m = (0,0)
+for i, j in enumerate(LCP):
+    if 0 <= suffix[i-1] + j - 1 < len(A) < suffix[i] + j - 1 < len(s):
+        m = max(m, (j,i))
+    if 0 <= suffix[i] + j - 1 < len(A) < suffix[i-1] + j - 1 < len(s):
+        m = max(m, (j,i))
+
+length, start = m
+print(length)
+print(s[suffix[start] : suffix[start] + length])
