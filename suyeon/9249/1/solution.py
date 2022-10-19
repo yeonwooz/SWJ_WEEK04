16b943ba30d3305a908ff64c0590d47a18da8cb7
suyeon woo <woosean999@gmail.com>
2022-10-19 19:29:41 +0900

    9249 최장 공통 부분 문자열 / LCP, LCS

--- suyeon/9249/1/solution.py
+++ suyeon/9249/1/solution.py
@@ -1,44 +1,46 @@
+# https://velog.io/@gopas777/%EB%B0%B1%EC%A4%80-9249-python
 import sys
 input = sys.stdin.readline
-
 A = input()
 B = input()
-
-s = A + '1' + B   #두 문자열을 구분짓기 위해 (인덱스가 넘어가지 않게) 특수문자나 숫자를 추가한 후 연결
+s = A + ':' + B  # 알파벳을 제외한 문자로 구분해준다
 n = len(s)
 
-suffix = [i for i in range(n)]
+# 접미사 배열
+sfa = [i for i in range(n)]  
+# 접미사 배열이란 어떤 문자열에 대해 인덱스 0 ~ n-1 각각에서부터 시작하는 접미사들을 나열한 후 사전순으로 배열한 것이다. 공간복잡도를 줄이기 위해 접미사 배열에는 해당 접미사 대신 그 접미사가 원래 문자열의 몇번째 인덱스부터 자른 것인지를 가리키는 정수 i(index number)가 들어간다
 
-g = [0] * (n+1)  # 순위
-ng = [0] * (n+1) # 갱신순위
+g = [0] * (n+1) # sfa[i], sfa[i+1]을 비교할 순위배열 (order)
+ng = [0] * (n+1) # g 를 재졍렬한 순위배열 (sorted order)
 
 for i in range(n):
     g[i] = ord(s[i])
+    # 파이썬의 ord 함수는 해당 char의 아스키 코드숫자를 반환하므로 정렬기준으로 사용할 수 있다
+    # A와 B를 합친 문자열 s를 아스키코드 기준으로 정렬한 순위배열 g
 
-g[n] = -1
-ng[suffix[0]] = 0 
-ng[n] = -1
+g[n] = -1  # 우선순위 배열 g의 마지막을 -1로 둠으로써 자동적으로 맨 앞으로 배치되도록 한다(?)
+ng[sfa[0]] = 0 # 사전순 첫번째 접미사의 우선순위는 0이며, ng에 저장
+ng[n] = -1  # g[n] 과 맞춰줌
 
-t = 1
+t = 1  # 조금씩 증가시킬 간격 (점점 더 길어짐)
 while t < n:
-    suffix.sort(key = lambda x: (g[x], g[min(x+t, n)]))
+    sfa.sort(key = lambda x: (g[x], g[min(x+t, n)])) # 현재 접미사랑, n미만에서 간격을 증가시킨 접미사의 우선순위를 기준으로 접미사 배열 정렬 
 
-    for i in range(1,n):
-        p,q = suffix[i-1], suffix[i] # 접미사 배열의 앞뒤 비교
+    for i in range(1, n):
+        p, q = sfa[i-1], sfa[i]
 
-        if g[p] != g[q] or g[min(p + t, n)] != g[min(q + t, n)]:
-            ng[q] = ng[p] + 1
-            # 우선순위 변경
+        if g[p] != g[q] or g[min(p+t, n)] != g[min(q+t, n)]:
+            # 접미사 배열의 앞뒤 우선순위가 다르면
+            ng[q] = ng[p] + 1 
+            # 뒤가 앞보다 크도록 정렬
         else:
             ng[q] = ng[p]
-            # 우선순위 그대로 받아옴
 
     if ng[n-1] == n-1:
         break
 
-
-    t = t * 2
-    g = ng[:]
+    t *= 2
+    g = ng[:]  # 우선순위 배열 g 최종갱신
 
 LCP = [0] * n
 length = 0
@@ -48,22 +50,24 @@ for i in range(n):
     if k == 0:
         # 처음은 들어가지 않는다
         continue
-    p = suffix[k-1]
+    p = sfa[k-1]   # i 인덱스부터 시작하는 접미사의 우선순위보다 하나 작은 (바로 앞의) 접미사 p
 
-    while i + length < n and p + length < n and s[i + length] ==  s[p + length]:
+    while i + length < n and p + length < n and s[i+length] == s[p + length]: 
+        # 접미사 i와 접미사p는 같은 문자일까?
         length += 1
     LCP[k] = length
 
     if length:
         length -= 1
 
-m = (0,0)
-for i, j in enumerate(LCP):
-    if 0 <= suffix[i-1] + j - 1 < len(A) < suffix[i] + j - 1 < len(s):
+m = (0,0) # length, start_index
+
+for i , j in enumerate(LCP):
+    if 0 <= sfa[i-1] + j - 1 < len(A) < sfa[i] + j - 1 < len(s):   # 접미사 배열기준 앞,뒤 비교
         m = max(m, (j,i))
-    if 0 <= suffix[i] + j - 1 < len(A) < suffix[i-1] + j - 1 < len(s):
+    if 0 <= sfa[i] + j - 1 < len(A) < sfa[i-1] + j - 1 < len(s):   # 접미사 배열기준 뒤,앞 비교
         m = max(m, (j,i))
 
-length, start = m
+length, i = m
 print(length)
-print(s[suffix[start] : suffix[start] + length])
+print(s[sfa[i]:  sfa[i] + length])  # 접미사의 i부터 길이만큼 자른 부분문자열
